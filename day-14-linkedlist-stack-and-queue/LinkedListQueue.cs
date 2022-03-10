593315ca2bc4684f9cd43ef6d750d5517d3e933c
ManojMichaelX0 <manojthiparapu@gmail.com>
2022-03-11 02:51:07 +0530

    added UC-4 Queue and Dequeue Problem

--- day-14-linkedlist-stack-and-queue/LinkedListQueue.cs
+++ day-14-linkedlist-stack-and-queue/LinkedListQueue.cs
@@ -8,5 +8,52 @@ namespace day_14_linkedlist_stack_and_queue
 {
     internal class LinkedListQueue
     {
+        Node head = null;
+        internal void Enqueue(int data)
+        {
+            Node node = new Node(data);
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into Queue", node.data);
+        }
+        internal void Dequeue(int data)
+        {
+            if (this.head == null)
+            {
+                Console.WriteLine("Queue is Empty");
+            }
+            Node temp = this.head;
+            this.head = this.head.next;
+            if(this.head == null)
+            {
+                this.head = null;
+            }
+            Console.WriteLine("Item Delete is {0}",temp.data);
+        }
+        internal void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Queue is Empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
     }
 }
--- day-14-linkedlist-stack-and-queue/Program.cs
+++ day-14-linkedlist-stack-and-queue/Program.cs
@@ -1,22 +1,41 @@
 ﻿using System;
+using System.Collections;
 
 namespace day_14_linkedlist_stack_and_queue
 {
     internal class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Pushing values to Stack");
-            LinkedListStack linkedListStack = new LinkedListStack();
-            linkedListStack.push(70);
-            linkedListStack.push(30);
-            linkedListStack.push(56);
-            linkedListStack.Display();
-            Console.WriteLine("Using Peek and Pop Till Stack is Empty");
-            linkedListStack.Peek();
-            linkedListStack.Pop();
-            linkedListStack.IsEmpty();
-            linkedListStack.Display();
+            //Console.WriteLine("Pushing values to Stack");
+            //LinkedListStack linkedListStack = new LinkedListStack();
+            //linkedListStack.push(70);
+            //linkedListStack.push(30);
+            //linkedListStack.push(56);
+            //linkedListStack.Display();
+           
+            //Console.WriteLine("Using Peek and Pop Till Stack is Empty");
+            //linkedListStack.Peek();
+            //linkedListStack.Pop();
+            //linkedListStack.IsEmpty();
+            //linkedListStack.Display();
+            
+            Console.WriteLine("Enqueue value in a Queue");
+            LinkedListQueue listQueue = new LinkedListQueue();
+            listQueue.Enqueue(56);
+            listQueue.Enqueue(30);
+            listQueue.Enqueue(70);
+            listQueue.Display();
+
+            Console.WriteLine("Dequeue Elements in Queue");
+            listQueue.Dequeue(56);
+            listQueue.Dequeue(36);
+            listQueue.Dequeue(70);
+            listQueue.Display();
+
+
+
 
         }
     }
