7a854cc7e3df33013cbc2f3cb61d5f489a68d917
ManojMichaelX0 <manojthiparapu@gmail.com>
2022-03-11 01:58:03 +0530

    added UC-1 Create Stack Using Linked List

---
+++ day-14-linkedlist-stack-and-queue/Node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day_14_linkedlist_stack_and_queue
+{
+    internal class Node
+    {
+        public int data;
+        public Node next;
+        public Node(int data)
+        {
+            this.data = data;
+        }
+    }
+}
---
+++ day-14-linkedlist-stack-and-queue/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace day_14_linkedlist_stack_and_queue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            LinkedListStack linkedListStack = new LinkedListStack();
+            linkedListStack.push(70);
+            linkedListStack.push(30);
+            linkedListStack.push(56);
+            linkedListStack.Display();
+           
+        }
+    }
+}
